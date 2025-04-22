445c5273dea6a48bbb8de5de24aba33025e78b4a
CSHan-Berklee <chan6@berklee.edu>
2025-04-22 18:18:31 -0400

    Level 1 Complete

---
+++ level_one.py
@@ -0,0 +1,3 @@
+print("\"What do you call a fish with no eyes?\" I paused, puzzled, until he grinned and said, \"FSH!\"")
+print('\"What do birds hang out on Halloween?\", she said \"Tweets.\"')
+print("Line 1 \n Line 2 \n  Line 3\n Line 4 \nLine 5")
