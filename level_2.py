b89a944229d1933c5c25ae8a96b8e6e6a867dc55
CSHan-Berklee <chan6@berklee.edu>
2025-04-22 18:38:17 -0400

    Level 2 Complete

---
+++ level_2.py
@@ -0,0 +1,5 @@
+mix_of_stuff = [0, "zero", 0., False] 
+playlist_for_coding = ["Don't Stop Believing", "Holiday", "Little Wing", "Good Ridance", "GLHF"]
+print(playlist_for_coding[0])
+playlist_for_coding.append("Don't start Now")
+print(playlist_for_coding)
